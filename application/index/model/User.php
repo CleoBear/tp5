dfd0ea7dd7afd9ba3ed8f31ec466889b2ee41bb9
Cleo Chan <huey0228@gmail.com>
2019-11-27 14:38:10 +0800

    Add User Model

--- application/index/model/User.php
+++ application/index/model/User.php
@@ -2,8 +2,13 @@
 namespace app\index\model;
 
 use think\Model;
+use think\facade\Config;
 
 class User extends Model
 {
+    protected $table = 'think_user';
     
+    // 设置当前模型的数据库连接
+    protected $connection = 'db_1';
+    protected $pk = 'id';
 }
